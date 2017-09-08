9b2554f98abe94b802466628e538acf042da5786
Julija Lee <lee.julija@gmail.com>
2017-09-08 19:36:56 +0000

    fixed pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
 from flask import Blueprint
 from models.amenity import Amenity
 from models.city import City
@@ -8,6 +11,3 @@ from models.review import Review
 from models.state import State
 from models.user import User
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
