883d52c5c64aa9f900d08abc12d7d077a938b050
Julija Lee <lee.julija@gmail.com>
2017-09-09 02:14:34 +0000

    added CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify, Flask, make_response
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
