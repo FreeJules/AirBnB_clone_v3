31a5c97e7f227dcb715049763463189a46e94855
meghamohan <meghamohan1989@gmail.com>
2017-09-12 21:58:23 +0000

    task7 initial commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ from models.user import User
 from models import storage
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
+from api.v1.views.states import *
