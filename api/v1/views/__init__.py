9e71448a6574feb1013f4cf79ca663d43158572a
meghamohan <meghamohan1989@gmail.com>
2017-09-13 17:29:49 +0000

    adding states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,9 +5,12 @@ from models.city import City
 from models import storage
 from models.place import Place
 from models.review import Review
+from models.amenity import Amenity
 from models.state import State
+from models.city import City
 from models.user import User
 from models import storage
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.amenities import *
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,7 +10,7 @@ from os import getenv
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def viewAllStates():
     """
-        
+        view all state objects
     """
     states = [state.to_json() for state in storage.all("State").values()]
     return jsonify(states)
@@ -19,7 +19,7 @@ def viewAllStates():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def viewOneState(state_id=None):
     """
-        
+        Retrieves a State object
     """
     if state_id is None:
         abort(404)
@@ -33,7 +33,7 @@ def viewOneState(state_id=None):
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def deleteState(state_id=None):
     """
-        
+        Deletes a State object
     """
     if state_id is None:
         abort(404)
@@ -50,7 +50,7 @@ def deleteState(state_id=None):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def createState():
     """
-        
+       Creates a State 
     """
     reqJson = request.get_json()
     if reqJson is None:
@@ -63,9 +63,20 @@ def createState():
 
 
 @app_views.route('/states/<states_id>', methods=['PUT'], strict_slashes=False)
-def updateState():
+def updateState(states_id=None):
     """
-        
+        updates a State object
     """
-    
+    reqJson = request.get_json()
+    if reqJson is None:
+        return "Not a JSON", 400
+    state = storage.get("State", states_id)
+    if state is None:
+        abort(404)
+    for fields in ("id", "created_at", "updated_at"):
+        reqJson.pop(fields, None)
+    for k,v in reqJson.items():
+        setattr(state, k, v)
+    state.save()
+    return jsonify(state.to_json()), 200
 
